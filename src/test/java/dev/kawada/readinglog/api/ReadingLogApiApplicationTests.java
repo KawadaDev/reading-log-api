2a53cfe4cb9081c3c50907708e08dabf04f337b1
Kawada.dev <254181471+KawadaDev@users.noreply.github.com>
2026-01-11 19:57:03 +0900

    Initial commit

---
+++ src/test/java/dev/kawada/readinglog/api/ReadingLogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kawada.readinglog.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingLogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
